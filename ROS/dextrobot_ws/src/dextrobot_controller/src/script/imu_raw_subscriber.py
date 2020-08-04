183503cda8cc2520218b85ba46967aafe08b0363
AntoBrandi <antonio.brandi@outlook.it>
2020-08-04 09:34:47 +0000

    first working version of the python imu reader and converter

--- ROS/dextrobot_ws/src/dextrobot_controller/src/script/imu_raw_subscriber.py
+++ ROS/dextrobot_ws/src/dextrobot_controller/src/script/imu_raw_subscriber.py
@@ -14,26 +14,22 @@ def callback(data):
     global pub
     readings = data.data.split(",")
     sensor_imu = Imu()
-    quaternion = Quaternion()
-    lin_acc = Vector3()
     # compose the message
     sensor_imu.header.frame_id = "imu"
     sensor_imu.header.stamp = rospy.Time.now()
-    cy = math.cos(readings[5] * 0.5)
-    sy = math.sin(readings[5] * 0.5)
-    cp = math.cos(readings[4] * 0.5)
-    sp = math.sin(readings[4] * 0.5)
-    cr = math.cos(readings[3] * 0.5)
-    sr = math.sin(readings[3] * 0.5)
-    quaternion.x = sr * cp * cy - cr * sp * sy
-    quaternion.y = cr * sp * cy + sr * cp * sy
-    quaternion.z = cr * cp * sy - sr * sp * cy
-    quaternion.w = cr * cp * cy + sr * sp * sy
-    sensor_imu.orientation = quaternion
-    lin_acc.x = readings[0]
-    lin_acc.y = readings[1]
-    lin_acc.z = readings[2]
-    sensor_imu.linear_acceleration = lin_acc
+    cy = math.cos(float(readings[5]) * 0.5)
+    sy = math.sin(float(readings[5]) * 0.5)
+    cp = math.cos(float(readings[4]) * 0.5)
+    sp = math.sin(float(readings[4]) * 0.5)
+    cr = math.cos(float(readings[3]) * 0.5)
+    sr = math.sin(float(readings[3]) * 0.5)
+    sensor_imu.orientation.x = sr * cp * cy - cr * sp * sy
+    sensor_imu.orientation.y = cr * sp * cy + sr * cp * sy
+    sensor_imu.orientation.z = cr * cp * sy - sr * sp * cy
+    sensor_imu.orientation.w = cr * cp * cy + sr * sp * sy
+    sensor_imu.linear_acceleration.x = float(readings[0])
+    sensor_imu.linear_acceleration.y = float(readings[1])
+    sensor_imu.linear_acceleration.z = float(readings[2])
     # publish the composed message
     pub.publish(sensor_imu)
 
