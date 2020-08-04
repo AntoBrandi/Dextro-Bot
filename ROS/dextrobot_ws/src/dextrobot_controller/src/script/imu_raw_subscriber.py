bd7ebecb44d46534c2381102adfe6d010824584a
anton <antonio.brandi@outlook.it>
2020-08-04 12:32:17 +0200

    python node conversion from RPY to quaternion

--- ROS/dextrobot_ws/src/dextrobot_controller/src/script/imu_raw_subscriber.py
+++ ROS/dextrobot_ws/src/dextrobot_controller/src/script/imu_raw_subscriber.py
@@ -2,8 +2,7 @@
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Quaternion
-from geometry_msgs.msg import Vector3
+from tf.transformations import quaternion_from_euler
 import math
 
 
@@ -17,16 +16,13 @@ def callback(data):
     # compose the message
     sensor_imu.header.frame_id = "imu"
     sensor_imu.header.stamp = rospy.Time.now()
-    cy = math.cos(float(readings[5]) * 0.5)
-    sy = math.sin(float(readings[5]) * 0.5)
-    cp = math.cos(float(readings[4]) * 0.5)
-    sp = math.sin(float(readings[4]) * 0.5)
-    cr = math.cos(float(readings[3]) * 0.5)
-    sr = math.sin(float(readings[3]) * 0.5)
-    sensor_imu.orientation.x = sr * cp * cy - cr * sp * sy
-    sensor_imu.orientation.y = cr * sp * cy + sr * cp * sy
-    sensor_imu.orientation.z = cr * cp * sy - sr * sp * cy
-    sensor_imu.orientation.w = cr * cp * cy + sr * sp * sy
+
+    q = quaternion_from_euler(readings[3], readings[4], readings[5])
+
+    sensor_imu.orientation.x = q[0]
+    sensor_imu.orientation.y = q[1]
+    sensor_imu.orientation.z = q[2]
+    sensor_imu.orientation.w = q[3]
     sensor_imu.linear_acceleration.x = float(readings[0])
     sensor_imu.linear_acceleration.y = float(readings[1])
     sensor_imu.linear_acceleration.z = float(readings[2])
